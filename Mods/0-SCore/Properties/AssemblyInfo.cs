3fe772ea25a03eb9b288782fbf18a1e6db499a2d
arramus <71211641+arramus@users.noreply.github.com>
2025-09-21 03:43:31 +0900

    Update
    - Adding replacement Workbench for carpentry rollback
    - Updating 0-SCore to 2.3.35.1444

--- Mods/0-SCore/Properties/AssemblyInfo.cs
+++ Mods/0-SCore/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("20.0.*")]
 
-[assembly: AssemblyVersion("2.3.30.1720")]
-[assembly: AssemblyFileVersion("2.3.30.1720")]
+[assembly: AssemblyVersion("2.3.35.1444")]
+[assembly: AssemblyFileVersion("2.3.35.1444")]
